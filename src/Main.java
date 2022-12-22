4709bd78133ffaf9b9cbc5eb1bb277fecc92e6e3
Alexandr Afanasev <116384401+AfanasBlack@users.noreply.github.com>
2022-12-22 11:44:24 +0300

    Добавлен eraser

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -8,12 +9,17 @@ public class Main {
 
         List<Person> people = new ArrayList<>();
 
+        people.add(new Person("Алексей", "Иванов", 13));
         people.add(new Person("Игорь", "Сафронов", 10));
         people.add(new Person("Юра", "Сафронов", 35));
         people.add(new Person("Александр", "Сухов", 25));
         people.add(new Person("Александр", "Сухов", 65));
         people.add(new Person("Гриша", "Аббакумовский68", 42));
         people.add(new Person("Гриша", "Аббакумовский", 43));
+
+        Predicate<Person> min18 = person -> person.getAge() < 18;
+
+        people.removeIf(min18);
 
         Collections.sort(people, new PersonsNobilityComparator(12));
 
